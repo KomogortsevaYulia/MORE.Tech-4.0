567d1ab13dfadb5be90daf5e884de26f9f81b1dd
PaaAngk <paaAngarsk@yandex.ru>
2022-10-08 02:35:03 +0800

    transaction

---
+++ client/src/pages/ActivitiesPage/ActivitiesPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CardTransactions from "../../components/TransactionsCard/CardTransactions";
+
+const ActivitiesPage = () => {
+  return <div>
+    <CardTransactions />
+  </div>;
+};
+
+export default ActivitiesPage;
