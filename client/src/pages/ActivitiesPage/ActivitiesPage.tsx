3e9c0af59d3f74c74e59434f8cf7ff5c9449c7a1
Yulia Komogortseva <komog-y-v@yandex.ru>
2022-10-08 05:01:43 +0800

    фикс percohal

--- client/src/pages/ActivitiesPage/ActivitiesPage.tsx
+++ client/src/pages/ActivitiesPage/ActivitiesPage.tsx
@@ -6,6 +6,9 @@ import Typography from '@mui/material/Typography';
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { fetchActivities } from "../../store/ActivitiesSlice/activitiesSlice";
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
 
 const ActivitiesPage = () => {
 
@@ -26,7 +29,7 @@ const ActivitiesPage = () => {
 
   const { Activities } = useAppSelector((state) => state.activities);
   React.useEffect(() => {
-    console.log(Activities);
+
   }, [Activities]);
 
   React.useEffect(() => {
@@ -35,28 +38,36 @@ const ActivitiesPage = () => {
 
 
   return <div>
-    {Activities && Activities?.map((row) => (
-      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            {row.title}
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>{row.date}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          {row.description}
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    ))}
+    <div>
+      <Stack spacing={2} sx={{ width: 300 }}>
+        <Autocomplete
+          id="free-solo-demo"
+          freeSolo
+          options={Activities && Activities?.map((option) => option.title) || []}
+          renderInput={(params) => <TextField {...params} label="Поиск" />}
+        />
+      </Stack>
 
-
-
+    </div>
+    <div>
+      {Activities && Activities?.map((row) => (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography> {row.title}</Typography>
+            <Typography sx={{ color: 'text.secondary' }}>{row.date}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+            {row.description}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </div>
   </div>;
 };
 
