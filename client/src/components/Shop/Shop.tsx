b9f2abb2bc7609d0140e02c26589f23745f2363c
PaaAngk <paaAngarsk@yandex.ru>
2022-10-08 12:38:21 +0800

    edt product card

--- client/src/components/Shop/Shop.tsx
+++ client/src/components/Shop/Shop.tsx
@@ -1,30 +1,100 @@
 import * as React from "react";
+import { IProduct } from "../../api/mainApi";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { fetchProducts } from "../../store/marketSlice/marketSlice";
 import ShopItem from "./ShopItem";
 
+
+
+
 export default function Shop() {
-    const dispatch = useAppDispatch();
-
-
-    const { products } = useAppSelector((state) => (state.market));
+	const dispatch = useAppDispatch();
+	const { products } = useAppSelector((state) => (state.market));
+	const [filteredProducts, setFilteredProducts] = React.useState(products);
+	const [maxPrice, setMaxPrice] = React.useState(1);
     React.useEffect(() => {
-        console.log(products);
-      }, [products]);
+		if(products){
+			setMaxPrice(Math.max(...products!.map(item => item.priceRuble)));
+			setPriceMax(maxPrice)
+			filterProducts([null]);
+		}
+    }, [products]);
+
+    React.useEffect(() => {
+        dispatch(
+			fetchProducts()
+		);
+    }, []);
+
+	const filterProducts = (query : any[]) => {
+		if (query[1] === 0){
+			setPriceMax(maxPrice);
+		}
+
+		else if (query[0] === null){ 
+			return setFilteredProducts(products)
+		}
+		
+		setFilteredProducts(
+			products!.filter( function(el:IProduct){
+				return el.priceRuble >= query[0] && 
+				el.priceRuble <= query[1]  
+			})
+		)
+	}
+
+	const [priceMin, setPriceMin] = React.useState(0);
+	const handleChangePriceMin = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setPriceMin(Number(event.target.value));
+		filterProducts([priceMin,priceMax])
+	};
+
+	const [priceMax, setPriceMax] = React.useState(maxPrice);
+	const handleChangePriceMax = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setPriceMax(Number(event.target.value));
+		filterProducts([priceMin,priceMax])
+	};
+
+	
+
     
-      React.useEffect(() => {
-        dispatch(fetchProducts());
-      }, []);
 
   return (
-        <div className="text-center container py-5">
+    <div className="container-fluid">
+      <h4 className="mb-4 text-center">Market</h4>
+      <div className="row">
+        <div className="col-lg-3">
+            <div className="card">
+                  <div className="card-body">
+					<div className="mb-4 card-title">Filter</div>
+					<div>
+						<h5 className="mb-3">Price</h5>
 
-        {products && products?.map((item) => (
+						<div className="">
+							<label className="form-label">Price</label>
 
-            
-            <ShopItem shopItem={item}/>
+							<div className="input-group">
+								<input id="minPrice" type="number" className="form-control" placeholder="Min." value={priceMin} onChange={handleChangePriceMin}></input>
+								<span className="input-group-text">to</span>
+								<input id="maxPrice" type="number" className="form-control" placeholder="Max." value={priceMax} onChange={handleChangePriceMax}></input>
+							</div>
+						</div>
 
-        ))}
+                    </div>
+                    <div className="mt-4 pt-3">
+                          <h5 className="mb-3">Price</h5>
+                    </div>
+                </div>
+            </div>
         </div>
+
+			<div className="row g-2 col-lg-9 ">
+				{filteredProducts && filteredProducts?.map((item) => (
+					<ShopItem shopItem={item}/>
+
+				))}
+			</div>
+        </div>
+    </div>
   );
 }
--- client/src/components/Shop/ShopItem.tsx
+++ client/src/components/Shop/ShopItem.tsx
@@ -10,29 +10,20 @@ const ShopItem : React.FC<IShopItemProps> = ({
 }) => {
 
   return (
-    <div className="col-lg-4 col-md-12 mb-4">
+    <div className="col-sm-6 col-lg-4">
+        
         
         <div className="card rounded">
-            <div className="bg-image hover-zoom ripple ripple-surface ripple-surface-light"
+            <div className="bg-image"
                 data-mdb-ripple-color="light">
-                <img src={shopItem.image}
-                className="w-100" />
-                <a href="#!">
-                {/* <div className="mask">
-                    <div className="d-flex justify-content-start align-items-end h-100">
-                    <h5><span className="badge bg-primary ms-2">New</span></h5>
-                    </div>
-                </div> */}
-                <div className="hover-overlay">
-                    <div className="mask" ></div>
-                </div>
-                </a>
+                <img src={shopItem.image} className="w-100" />
+
             </div>
-            <div className="card-body">
+            <div className="card-inner text-center">
                 <a href="" className="text-reset">
-                <h5 className="card-title mb-3">{shopItem.title}</h5>
+                    <h5 className="card-title mb-3">{shopItem.title}</h5>
                 </a>
-                <h6 className="mb-3">{shopItem.priceRuble}</h6>
+                <h6 className="mb-3 mx-auto">{shopItem.priceRuble}</h6>
             </div>
         </div>
     </div>
