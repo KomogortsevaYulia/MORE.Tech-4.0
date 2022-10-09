5205aa0d30ea5577e0aed146996cb55b6bb6b1ea
PaaAngk <paaAngarsk@yandex.ru>
2022-10-09 09:11:51 +0800

    fetchUserInDepartments

--- client/src/api/mainApi.ts
+++ client/src/api/mainApi.ts
@@ -209,7 +209,7 @@ export class MainApi {
       .then((response) => response.data);
 
     const usersActivities = await axios
-      .get<IActivityRecords[]>(`${apiUrl}/activity_records?&_expand=user&_expand=activities`)
+      .get<IActivityRecords[]>(`${apiUrl}/users?&_expand=departments`)
       .then((response) => response.data);
 
     for (let activity of activities) {
@@ -217,9 +217,27 @@ export class MainApi {
         (userActivity) => userActivity.activitiesId === activity.id,
       );
     }
-
     return activities;
   }
+
+  // static async fetchUserInDepartments(userId: number) {
+  //   const activities = await axios
+  //     .get<IActivities[]>(`${apiUrl}/activities`)
+  //     .then((response) => response.data);
+
+  //   const usersActivities = await axios
+  //     .get<IActivityRecords[]>(`${apiUrl}/activity_records?&_expand=user&_expand=activities`)
+  //     .then((response) => response.data);
+
+  //   for (let activity of activities) {
+  //     activity.users = usersActivities.filter(
+  //       (userActivity) => userActivity.activitiesId === activity.id,
+  //     );
+  //   }
+
+  //   return activities;
+  // }
+
 
   static async fetchActivitiesForHome() {
     const activities = await axios
